7305ae80aa489f967045fc120c8affdea6c64c1d
huntermaxfield <huntermaxfield1@gmail.com>
2015-07-28 19:19:42 -0600

    done oh yeah

--- service.js
+++ service.js
@@ -1,12 +1,21 @@
 var app = angular.module('userProfiles');
 
-app.service('mainService', function($http) {
+app.service('mainService', function($http, $q) {
 
   this.getUsers = function() {
-    return $http({
+    var dfd = $q.defer();
+    $http({
         method: 'GET',
         url: 'http://reqr.es/api/users?page=1'
+    }).then(function(data){
+        var parsedResponse = data.data.data;
+        for(var i = 0; i < parsedResponse.length; i++) {
+            parsedResponse[i].first_name = 'Ralf'
+        }
+        dfd.resolve(parsedResponse);
     })
+    return dfd.promise;
+
   }
 
 });
